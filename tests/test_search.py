b68f06699e8b9c4c20178beddb527300655d9784
yuchou <xfufo@qq.com>
2017-04-23 20:14:33 +0800

    [A] 增加配置文件

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,12 +11,14 @@ class Test_Search(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(10)
+        self.base_url = config.baseUrl
+        self.driver.maximize_window()
 
     def tearDown(self):
         self.driver.close()
 
     def test_search(self):
-        self.driver.get(config.baseUrl)
+        self.driver.get(self.base_url)
         home_page = homepage.HomePage(self.driver)
         home_page.input_search('test')
         home_page.click_search()
