b860d99d18385db922bf737306ec40f3bc008a27
zouyu <zouyu/user@youlu>
2017-05-05 17:18:48 +0800

    [A] 新增邮件发送模块

---
+++ comm/mail/sendmail.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def smail(report_file, to_list):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = "[有路科技]项目测试报告"
+    msg['From'] = "admin/user@youlu"
+    msg['To'] = ",".join(to_list)
+    for files in report_file:
+        att = MIMEText(open(files, 'rb').read(), 'base64', 'utf-8')
+        att["Content-Type"] = 'application/octet-stream'
+        att.add_header('content-disposition', 'attachment', filename=files)
+        msg.attach(att)
+        content = MIMEText(open(files, encoding="utf-8").read(), 'html', 'utf-8')
+        msg.attach(content)
+
+    s = smtplib.SMTP('10.10.0.6')
+    s.send_message(msg)
+    s.quit()
+
+
+if __name__ == '__main__':
+    file_new = ["Test_TestStringMethods_2017-05-05_16-17.html", "Test_test_demo.Test_Demo_2017-04-26_15-05.html"]
+    to_list = ["zouyu/user@youlu", "zhoulin/user@youlu", "lulanfang/user@youlu"]
+    smail(file_new, to_list)
